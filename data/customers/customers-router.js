b1780ade6e0aa9559712a1e01fcacf6e4727cc40
Trevor Thompson <Thompson_Trevor@bah.com>
2020-04-24 08:16:00 -0400

    Added code to fix server startup as well as resource endpoint configuration

--- data/customers/customers-router.js
+++ data/customers/customers-router.js
@@ -6,54 +6,78 @@ const router = express.Router();
 router.get("/:id", async (req, res, next) => {
 
     try {
-        const customers = await db("customers").where("id", req.params.id).first();
-        res.json(customers);
-    } catch(error) {
+        const accounts = await db("accounts").where("id", req.params.id).first();
+        res.json(accounts);
+    } catch (error) {
         next(error)
     }
 
 })
 
-router.get("/",  async (req, res, next) => {
+router.get("/", async (req, res, next) => {
 
     try {
-        const customers = await db.select("*").from("customers");
-        res.json(customers);
-    } catch(error) {
+        const accounts = await db.select("*").from("accounts");
+        res.json(accounts);
+    } catch (error) {
         next(error);
     }
 
 })
 
-router.post("/",  async (req, res, next) => {
+router.post("/", async (req, res, next) => {
 
     try {
         const payload = {
+
+            name: req.body.name,
+            budget: req.body.budget
             // need to figure out how to push the payloads to this thing
         }
-    } catch(error) {
+
+        const [id] = await db("accounts").insert(payload);
+        const account = await db("accounts").where("id", id).first();
+
+        res.json(account);
+
+    } catch (error) {
         next(error)
     }
 
 })
 
-router.delete("/:id ", async (req, res, next) => {
+router.delete("/:id", async (req, res, next) => {
 
     try {
 
-        const payload = {
-            
-        }
+        await db("accounts").where("id", req.params.id).del()
+        res.status(204).end();
 
-    } catch(error) {
+    } catch (error) {
         next(error);
     }
 
 })
 
-router.put("/:id",  async (req, res, next) => {
+router.put("/:id", async (req, res, next) => {
+
+    try {
+        const payload = {
+
+            name: req.body.name,
+            budget: req.body.budget
+            // need to figure out how to push the payloads to this thing
+        }
+
+        await db("accounts").where("id", req.params.id).update(payload);
+        const accountMessage = await db("accounts").where(id, req.params.id);
+
+        res.json(accountMessage);
+
+    } catch (error) {
+        next(error);
+    }
 
 })
-
 
 module.exports = router;
--- queries.sql
+++ queries.sql
@@ -31,4 +31,6 @@ WHERE
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
+
+
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
